9920d5871e245056350de8e718f94fd5e601c78a
Ben Jeske <ben@fortifiedhq.com>
2021-08-19 10:50:21 -0400

    api util urls

---
+++ utils/api.js
@@ -0,0 +1,5 @@
+export const BASE_URL = "https://swapi.dev/api/";
+export const CHARACTER_URL = "people/"
+export const PLANETS_URL = "planets/"
+export const STARSHIPS_URL = "starships/"
+export const FILMS_URL = "films/"
